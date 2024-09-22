0e26b6fef20c8cfa5a20ac28810a100b653bc90c
i-Milman <ivan.milman.arkhangelsk@ya.ru>
2024-09-22 03:00:36 +0300

    Обновлен основной код
    - увеличена точность попадания
    - кликать по монете перестало по непонятной причине, поэтому заменил клики просто на передвижение мыши, использовать в связке с GS Auto Clicker пока проблема не будет решена

--- ecc_clicker.py
+++ ecc_clicker.py
@@ -3,15 +3,14 @@ import numpy as np
 import cv2
 import keyboard
 
-
-def find_color_and_click(region, target_color, stop_key='ctrl', delay=10):
+def find_color(region, target_color, stop_key='ctrl', delay=100):
     # Установка области поиска
     x, y, width, height = region
 
     # Определение диапазона цвета в формате BGR
-    target_color_bgr = (target_color[2], target_color[1], target_color[0])  # Конвертация RGB в BGR
-    lower_bound = np.array([target_color_bgr[0] - 10, target_color_bgr[1] - 10, target_color_bgr[2] - 10])
-    upper_bound = np.array([target_color_bgr[0] + 10, target_color_bgr[1] + 10, target_color_bgr[2] + 10])
+    target_color_bgr = np.array([target_color[2], target_color[1], target_color[0]])  # Конвертация RGB в BGR
+    lower_bound = target_color_bgr - 10
+    upper_bound = target_color_bgr + 10
 
     while True:
         # Управление циклом
@@ -35,29 +34,33 @@ def find_color_and_click(region, target_color, stop_key='ctrl', delay=10):
         if contours:
             largest_contour = max(contours, key=cv2.contourArea)
             M = cv2.moments(largest_contour)
-            if M["m00"] != 0:
+            if M["m00"] > 200:
                 cX = int(M["m10"] / M["m00"]) + x
                 cY = int(M["m01"] / M["m00"]) + y
-                pyautogui.click(cX, cY)
-                print(f"Кликнули по цвету в ({cX}, {cY})")
+                pyautogui.moveTo(cX, cY)
+                print(f"Мышь на цвете в ({cX}, {cY})")
             else:
-                click_center(region)
+                find_center(region)
         else:
-            click_center(region)
+            find_center(region)
+        # pyautogui.click()
 
 
-def click_center(region):
+def find_center(region):
     x, y, width, height = region
     center_x = x + width // 2
     center_y = y + height // 2
-    pyautogui.click(center_x, center_y)
-    print(f"Цвет не найден, кликнули в центре области ({center_x}, {center_y})")
+    # pyautogui.click(center_x, center_y)
+    pyautogui.moveTo(center_x, center_y)
+    print(f"Цвет не найден, мышь в центре области ({center_x}, {center_y})")
 
 
 if __name__ == "__main__":
     # Укажите область поиска: (x, y, ширина, высота)
-    region = (335, 220, 320, 320)  # Область 320x320 пикселей, начиная с (335, 220)
+    region = (420, 320, 600, 500)  # Область 320x320 пикселей, начиная с (335, 220)
     # Укажите целевой цвет: (R, G, B)
     target_color = (147, 88, 49)  # Коричневый цвет
+    # target_color = (147, 147, 147)  # Серый цвет
+    # target_color = (255, 172, 51)  # Жёлтый цвет
 
-    find_color_and_click(region, target_color)
+    find_color(region, target_color)
